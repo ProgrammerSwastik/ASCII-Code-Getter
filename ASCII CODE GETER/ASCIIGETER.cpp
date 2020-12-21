19e227538b7266284b4b0a8d2ad81baa4d9f3904
Swastik Bhattacharjee <73767874+SwastikBhattacharjee@users.noreply.github.com>
2020-12-21 10:44:40 +0530

    Add files via upload

---
+++ ASCII CODE GETER/ASCIIGETER.cpp
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+int main()
+{
+	char one;
+	
+	printf("\n");
+	
+	printf("********************************WELCOME TO THE ASCII CODE GETER*******************************\n");
+	
+	printf("\n");
+	printf("\n");
+	
+	printf("Enter the character of which you want the ASCII code:");
+	scanf("%c", & one);
+	
+	printf("\n");
+	
+	printf("The ASCII cde of your character is: %d", one);
+	
+	printf("\n");
+	printf("\n");
+	
+	printf("********************************BYE BYE*******************************\n");
+	
+	
+}
